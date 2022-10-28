d268a8212c4f3171a9e5276765512c6f27ef41f7
purdax <114296002+purdax@users.noreply.github.com>
2022-10-28 11:17:55 +0300

    Update button.js

--- button.js
+++ button.js
@@ -1,26 +1,23 @@
-<script>
- function AddButtons() {
-  var number = document.getElementById('numbers').value;
-  while (number != 0 && number > 0) {  
-    let btn = document.createElement("button"); 
-    btn.innerHTML = '<button id="button" onclick="Winner()"><img src="https://img.icons8.com/dotty/80/000000/resume-button.png"/></button>';
-    document.body.appendChild(btn);
-    --number;
+function addButtons() {
+  let number = document.getElementById('numbers').value;
+    for (let i = 0; i < number; ++i) {  
+      let btn = document.createElement("button"); 
+      btn.innerHTML = '<button id="button" onclick="winner()"><img src="https://img.icons8.com/dotty/80/000000/resume-button.png"/></button>';
+      document.body.appendChild(btn);
     }
 }
 
- function Winner() {
-  var total = document.getElementById('numbers').value;
-  var randomNumber = Math.floor(Math.random() * total + 1);
-  var WinnerButton = 1;
-  if (randomNumber == WinnerButton) {
-     alert("Winner! Congratulations!");
+function winner() {
+  let total = document.getElementById('numbers').value;
+  let randomNumber = Math.floor(Math.random() * total + 1);
+  let winnerButton = 1;
+    if (randomNumber == winnerButton) {
+      alert("Winner! Congratulations!");
     } else {
       alert("Please try again!")
     }
-  }
+}
 
-  function refreshPage() {
-    window.location.reload();
-    }
-</script>
+function refreshPage() {
+  window.location.reload();
+}
